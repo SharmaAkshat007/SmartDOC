b397fd8ca7456f6306896aea2560759c13375cc9
Shikhar03Stark <hv.harshit321@gmail.com>
2022-03-09 22:36:46 +0530

    Readability Score

---
+++ Http/server.py
@@ -0,0 +1,82 @@
+from datetime import datetime
+import os
+from time import time
+from flask import Flask, flash, request, redirect, url_for, jsonify
+from werkzeug.utils import secure_filename
+from werkzeug.exceptions import HTTPException
+import subprocess
+
+UPLOAD_FOLDER = 'temp/'
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/v1/findReadability', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            resp = jsonify({'message' : 'No file part in the request'})
+            resp.status_code = 400
+            return resp
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            resp = jsonify({'status': 400, 'message' : 'No file submitted'})
+            resp.status_code = 400
+            return resp
+        if file and allowed_file(file.filename):
+            filename = secure_filename(str(int(1000*time()))+file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # run readability cmd
+            # return redirect(url_for('download_file', name=filename))
+            stdout = subprocess.run(['../Readability/predict', '--docker-image', 'nima-cpu', '--base-model-name', 'MobileNet', '--weights-file', os.path.join(os.getcwd(), '..', 'Readability', 'models', 'MobileNet', 'weights_mobilenet_technical_0.11.hdf5'), '--image-source', os.path.join(os.getcwd(), app.config['UPLOAD_FOLDER'], filename)], capture_output=True)
+            # print('stdout:', stdout.stdout)
+            output = str(stdout.stdout)
+            output = output.split('\\n')
+            # print(output)
+            output = [line.strip() for line in output]
+            # print(output)
+            acc = float(str(output[4]).split()[-1])
+            resp = jsonify(
+                {
+                    'status': 200,
+                    'score': acc,
+                }
+            )
+            resp.status_code = 200
+            os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return resp
+
+
+@app.route('/')
+def index():
+    return {
+        "status": 200,
+        "message": "You are using DIQA++ REST APIs",
+    }
+
+
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    """Return JSON instead of HTML for HTTP errors."""
+    # start with the correct headers and status code from the error
+    # response = e.get_response()
+    # replace the body with JSON
+    response = jsonify({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    })
+    response.status = e.code
+    return response
+
+
+
